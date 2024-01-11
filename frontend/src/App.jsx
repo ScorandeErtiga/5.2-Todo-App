4313edae07aacda95fcdbd1d8f39de773bb3f7a3
ScorandeErtiga <67196995+ScorandeErtiga@users.noreply.github.com>
2024-01-11 18:27:57 +0530

    Almost done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,16 @@
 import { CreateTodo } from "./components/CreateTodo";
 import { Todos } from "./components/Todos";
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 function App() {
   const [todos, setTodos] = useState([]);
 
-  fetch("http://localhost:3000/todos")
-  .then(async function(data){
-    const response = await data.json();
-    //console.log(response);
-    setTodos(response.todos);
-  })
+  useEffect(function (){
+    fetch("http://localhost:3000/todos")
+    .then(async function(data){
+      const response = await data.json();
+      setTodos(response.todos);
+    })
+  },[])
 
   return (
     <div>
--- frontend/src/components/CreateTodo.jsx
+++ frontend/src/components/CreateTodo.jsx
@@ -1,16 +1,40 @@
+import { useState } from "react";
+
 export function CreateTodo(){
+    const [title, setTitle] = useState("");
+    const [ description, setDescription ] = useState("");
     return (
         <div>
-            <input style={{
+            <input onChange={function(e){
+                setTitle(e.target.value);
+            }} style={{
                 padding: 10,
                 margin: 10
             }} type="text" placeholder="Title"></input><br />
-            <input style={{
+
+            <input onChange={function(e){
+                setDescription(e.target.value);
+            }} style={{
                 padding: 10,
                 margin: 10
             }} type="text" placeholder="Description"></input><br />
 
-            <button style={{
+            <button onClick={function(){
+                fetch("http://localhost:3000/todo",{
+                    method: "POST",
+                    body: JSON.stringify({
+                        title: title,
+                        description: description
+                    }),
+                    headers:{
+                        "Content-type": "application/json"
+                    }
+                })
+                    .then(async function(data){
+                        const json = await data.json();
+                        alert("Todo added");
+                    })
+            }} style={{
                 padding: 10,
                 margin: 10
             }}>Add Todo</button>
